de017db458788490d7275f9d79f30870b67d675d
木村　圭一郎 <kimurakeiichirou@kimurakeiichirounoMacBook-Pro.local>
2019-06-18 18:23:19 +0900

    usershow画面設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :users
+  #resources :users
   devise_for :users
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
