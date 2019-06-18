897cbf2bbd0305fe9621c76b1ccb317c0cbd4f58
木村　圭一郎 <kimurakeiichirou@kimurakeiichirounoMacBook-Pro.local>
2019-06-18 19:48:21 +0900

    user機能route設定、show画面作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,12 @@
 class ApplicationController < ActionController::Base
   #before_action :authenticate_user!         
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  
   protected
   def configure_permitted_parameters
     # devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     # devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-    added_attrs = [ :username, :email, :password, :password_confirmation]
+    added_attrs = [ :name, :email, :password, :password_confirmation]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@
 
 Rails.application.routes.draw do
   #resources :users
-  devise_for :users
+  devise_for :users, controllers: {
+        registrations: 'users/registrations'
+}
   resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
