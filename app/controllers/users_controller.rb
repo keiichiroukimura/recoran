14f38a4d61f264e4ab72ee0db34344891bf8c28e
木村　圭一郎 <kimurakeiichirou@kimurakeiichirounoMacBook-Pro.local>
2019-06-23 15:42:50 +0900

    feat: コメント名前表示追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,5 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id]) 
     @posts = Post.where(user_id: @user.id).all
-    byebug
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,6 @@ class Post < ApplicationRecord
   has_many :joins, dependent: :destroy
   has_many :musics, through: :joins, source: :music
   has_many :comments, dependent: :destroy
-  has_many :users, through: :comments, source: :user
+  #has_many :users, through: :comments, source: :user
   belongs_to :user
 end
