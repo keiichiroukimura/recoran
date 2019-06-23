dbf0aca4c7e7b38afc9f07f9bd0fa62b15c34076
木村　圭一郎 <kimurakeiichirou@kimurakeiichirounoMacBook-Pro.local>
2019-06-23 17:10:13 +0900

    fix: 曲保存バグ修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id]) 
-    @posts = Post.where(user_id: @user.id).all
+    @posts = Post.where(user_id: @user.id).all.order(created_at: "DESC")
   end
 end
