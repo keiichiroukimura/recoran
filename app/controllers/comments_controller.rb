324d54f1dd01c261e3b4202ad8be27171a495f2f
木村　圭一郎 <kimurakeiichirou@kimurakeiichirounoMacBook-Pro.local>
2019-06-23 14:28:33 +0900

    feat: comment MVC追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
     # Blogをパラメータの値から探し出し,Blogに紐づくcommentsとしてbuildします。
     @post = Post.find(params[:post_id])
     @comment = @post.comments.build(comment_params)
-   
+    
     respond_to do |format|
       if @comment.save
         format.js { render :index }
