5db196a157deaa4faa0554c19cd79a9ef61cda3f
木村　圭一郎 <kimurakeiichirou@kimurakeiichirounoMacBook-Pro.local>
2019-06-24 17:10:50 +0900

    feat: music_search画面レスポンシブ対応

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,55 +15,55 @@
 //= require turbolinks
 //= require jquery/dist/jquery.js
 //= require_tree .
-// $(function(){
-//   $fileField = $('#file')
-  // 選択された画像を取得し表示
-//   $($fileField).on('change', $fileField, function(e) {
-//     file = e.target.files[0]
-//     reader = new FileReader(),
-//     $preview = $("#img_field");
-
-//     reader.onload = (function(file) {
-//       return function(e) {
-//         $preview.empty();
-//         $preview.append($('<img>').attr({
-//           src: e.target.result,
-//           width: "100%",
-//           class: "preview",
-//           title: file.name
-//         }));
-//       };
-//     })(file);
-//     reader.readAsDataURL(file);
-//   });
-// });
-
 $(function(){
-  //画像ファイルプレビュー表示のイベント追加 fileを選択時に発火するイベントを登録
-  $('form').on('change', 'input[type="file"]', function(e) {
-    var file = e.target.files[0],
-        reader = new FileReader(),
-        $preview = $("#img_field");
-        t = this;
-    // 画像ファイル以外の場合は何もしない
-    if(file.type.indexOf("image") < 0){
-      return false;
-    }
-    // ファイル読み込みが完了した際のイベント登録
+  $fileField = $('#file')
+  選択された画像を取得し表示
+  $($fileField).on('change', $fileField, function(e) {
+    file = e.target.files[0]
+    reader = new FileReader(),
+    $preview = $("#img_field");
+
     reader.onload = (function(file) {
       return function(e) {
-        //既存のプレビューを削除
         $preview.empty();
-        // .prevewの領域の中にロードした画像を表示するimageタグを追加
         $preview.append($('<img>').attr({
-                  src: e.target.result,
-                  width: "100%",
-                  class: "#img_field",
-                  title: file.name
-              }));
+          src: e.target.result,
+          width: "100%",
+          id: "#img_field",
+          title: file.name
+        }));
       };
     })(file);
     reader.readAsDataURL(file);
   });
 });
+
+// $(function(){
+//   //画像ファイルプレビュー表示のイベント追加 fileを選択時に発火するイベントを登録
+//   $('form').on('change', 'input[type="file"]', function(e) {
+//     var file = e.target.files[0],
+//         reader = new FileReader(),
+//         $preview = $("#img_field");
+//         t = this;
+//     // 画像ファイル以外の場合は何もしない
+//     if(file.type.indexOf("image") < 0){
+//       return false;
+//     }
+//     // ファイル読み込みが完了した際のイベント登録
+//     reader.onload = (function(file) {
+//       return function(e) {
+//         //既存のプレビューを削除
+//         $preview.empty();
+//         // .prevewの領域の中にロードした画像を表示するimageタグを追加
+//         $preview.append($('<img>').attr({
+//                   src: e.target.result,
+//                   width: "100%",
+//                   class: "#img_field",
+//                   title: file.name
+//               }));
+//       };
+//     })(file);
+//     reader.readAsDataURL(file);
+//   });
+// });
 
