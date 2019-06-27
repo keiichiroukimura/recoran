e131e9d082d1642d5bfa5dd22199919659881e05
木村　圭一郎 <kimurakeiichirou@kimurakeiichirounoMacBook-Pro.local>
2019-06-27 20:45:52 +0900

    feat: post_modelテスト修正

--- spec/model/post_spec.rb
+++ spec/model/post_spec.rb
@@ -2,12 +2,12 @@ require "rails_helper"
 
 RSpec.describe Post, type: :model do
   context 'Post登録のテスト' do
-    it 'contentが空ならバリデーションが通らない' do
-      post = FactoryBot.build(:post, content: '')
+    it 'user_idが空ならバリデーションが通らない' do
+      post = FactoryBot.build(:post)
       expect(post).not_to be_valid
     end
     
-    it "contentに内容が記載されていればバリデーションが通る" do
+    it "user_idがあればバリデーションが通る" do
       user_a = FactoryBot.create(:user)
       post = Post.new(content: 'テスト成功', user: user_a)
       expect(post).to be_valid
