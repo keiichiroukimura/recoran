b023182790d80ab544c240509bffbc95d118fb2b
木村　圭一郎 <kimurakeiichirou@kimurakeiichirounoMacBook-Pro.local>
2019-06-27 19:51:01 +0900

    feat: music_modelテスト実装

--- app/models/music.rb
+++ app/models/music.rb
@@ -3,4 +3,7 @@
 class Music < ApplicationRecord
   has_many :togethers, dependent: :destroy
   has_many :posts, through: :togethers, source: :post
+  validates :artist_name, presence: true
+  validates :track_name, presence: true
+  validates :album_name, presence: true
 end
--- spec/model/music_spec.rb
+++ spec/model/music_spec.rb
@@ -7,5 +7,17 @@ RSpec.describe Music, type: :model do
       music = FactoryBot.create(:music)
       expect(music).to be_valid
     end
+    it 'artist_nameが空ならバリデーションが通らない' do
+      music = FactoryBot.build(:music, artist_name: '')
+      expect(music).not_to be_valid
+    end
+    it 'track_nameが空ならバリデーションが通らない' do
+      music = FactoryBot.build(:music, track_name: '')
+      expect(music).not_to be_valid
+    end
+    it 'album_nameが空ならバリデーションが通らない' do
+      music = FactoryBot.build(:music, album_name: '')
+      expect(music).not_to be_valid
+    end
   end
 end
