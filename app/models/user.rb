ca952d6a23373e67b80463bfb1457c7472d6d632
木村　圭一郎 <kimurakeiichirou@kimurakeiichirounoMacBook-Pro.local>
2019-07-05 11:31:40 +0900

    feat: OAuthコード修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable,  :validatable,:omniauthable, omniauth_providers: %i(google)
+         :recoverable, :rememberable, :validatable,:omniauthable, omniauth_providers: %i(google)
   validates :name, presence: true, length: { maximum: 20 }
   validates :email, presence: true, length: { maximum: 255 },
       format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
